6dd8735e145c14012bfaeaec0131fc5a0fa13249
vikasrushi <0xvikas@gmail.com>
2022-09-05 02:12:08 +0530

    5

--- 5.b.py
+++ 5.b.py
@@ -23,3 +23,8 @@ for i in range(rows,0,-1):
     for j in range(0,i-1):
         print("*",end=" ")
     print()
+# # Lower Part
+# for i in range(rows,0,-1):
+#     for j in range(0,i-1):
+#         print("*",end=" ")
+#     print()
